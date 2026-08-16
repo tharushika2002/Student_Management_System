3d152abf88a4fb01d7186277d4b1eb27c2e20907
Tharushika Thilakarathna <sttharuu123@gmail.com>
2026-08-16 11:59:06 +0530

    feat(models): create Student database model

--- students/models.py
+++ students/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+class Student(models.Model):
+    # Student's full name
+    name = models.CharField(max_length=100)
 
-# Create your models here.
+    # Student's email address
+    email = models.EmailField()
+
+    # Student's age
+    age = models.IntegerField()
+
+    # Student's course name
+    course = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
