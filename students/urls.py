09d0b8a1e9b2a9875471713a44e01bc5bd7d6dc4
Tharushika Thilakarathna <sttharuu123@gmail.com>
2026-08-17 11:36:16 +0530

    feat: create and adding new html files for student form

--- students/urls.py
+++ students/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # URL for displaying all students
     path("", views.student_list, name="student_list"),
+    path("add/" , views.add_student, name="add_student"),
 ]
