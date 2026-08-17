9b8f55713beb29c5d27ab93b85d7d1e316ea4d2c
Tharushika Thilakarathna <sttharuu123@gmail.com>
2026-08-17 11:32:42 +0530

    feat: add function for student forms

--- students/views.py
+++ students/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student
+from .forms import StudentForm
 
 
 def student_list(request):
@@ -9,4 +10,19 @@ def student_list(request):
     # Send the student data to the HTML template
     return render(request, "students/student_list.html", {
         "students": students
+    })
+
+def add_student(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("student_list")
+
+    else:
+        form = StudentForm()
+
+    return render(request, "students/add_student.html", {
+        "form": form
     })
