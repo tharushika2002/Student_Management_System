e7d817c691ee02ba70423d01c45335f29f6a0701
Tharushika Thilakarathna <sttharuu123@gmail.com>
2026-08-17 10:28:23 +0530

    feat: create database conection

--- config/settings.py
+++ config/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "student_management",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
